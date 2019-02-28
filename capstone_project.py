8f250a5407e3bd000cd89ba2969bb7341b9973d7
vkatakkar <47355524+vkatakkar@users.noreply.github.com>
2019-02-28 12:54:11 +0530

    Add files via upload

---
+++ capstone_project.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
+
+# In[2]:
+
+
+pwd
+
+
+# In[1]:
+
+
+pwd
+
+
+# In[2]:
+
+
+ls
+
+
+# In[ ]:
+
+
+
+
